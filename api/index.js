0d0d5ee21dfdedb201253c1df84b4fbc9d34b5e8
Leonard Mocanu <leonard.mocanu@ymail.com>
2022-11-19 17:34:31 +0200

    adds api service and docker-compose

---
+++ api/index.js
@@ -0,0 +1,64 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+const express = require('express');
+const baucis = require('baucis');
+
+mongoose.connect(process.env.DB_URI);
+
+// Create a Mongoose schema
+const Patient = new mongoose.Schema({
+  client: String,
+  action: String,
+  resource: String,
+  time: Date,
+});
+// Note that Mongoose middleware will be executed as usual
+Patient.pre('save', function (next) {
+  console.log('A patient was saved to Mongo: %s.', this.get('client'));
+  next();
+});
+
+// Register the schema
+mongoose.model('patient', Patient);
+
+// Create dummy data
+var data = [
+  {
+    client: 'ASD0',
+    action: 'First Consult',
+    resource: 'Psychologist',
+    time: new Date('11/19/22'),
+  },
+  {
+    client: 'Teacher',
+    action: 'SelfAssement',
+    resource: 'Teacher',
+    time: new Date('11/19/22'),
+  },
+  {
+    client: 'Psychologist',
+    action: 'SelfAssement',
+    resource: 'Psychologist',
+    time: new Date('11/19/22'),
+  }
+];
+
+const main = async () => {
+  // Clear the database of old vegetables
+  await mongoose.model('patient').deleteMany();
+  await mongoose.model('patient').create(data);
+
+
+  // Create the API routes
+  baucis.rest('patient');
+
+  // Create the app and listen for API requests
+  var app = express();
+  app.use('/api', baucis());
+  app.listen(process.env.SERVER_PORT);
+
+  console.log(`Server listening on port ${process.env.SERVER_PORT}.`);
+};
+
+main();
