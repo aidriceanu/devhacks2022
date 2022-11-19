f96cf26b68129945e2a2b1969106ee1ac08e39f6
Leonard Mocanu <leonard.mocanu@ymail.com>
2022-11-19 21:27:28 +0200

    adds cors to api

--- api/index.js
+++ api/index.js
@@ -129,7 +129,17 @@ const main = async () => {
   baucis.rest('situation');
   baucis.rest('activity');
 
+  // Add cors support
+  const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, If-None-Match');
+    next();
+  }
+
+
   var app = express();
+  app.use(allowCrossDomain);
   app.use('/api', baucis());
   app.listen(process.env.SERVER_PORT);
 
